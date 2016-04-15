90a26c8cfc2324ca63288bb398d1c62a040f0cab
Brian Zelip <bzelip@gmail.com>
2016-04-15 16:43:51 -0400

    a little work on weather planning

--- local-weather/app.js
+++ local-weather/app.js
@@ -9,12 +9,12 @@ function geoShit() {
 
   function success(position) {
     var lat  = position.coords.latitude;
-    var long = position.coords.longitude;
+    var lon = position.coords.longitude;
 
-    content.innerHTML = "<p>hello motherfuckers!!!</p>";
+    content.innerHTML = "<p>Your latitude is: " + lat + "</p><p>Your longitude is: " + lon + "</p>";
 
     var img = new Image();
-    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + long + "&zoom=13&size=300x300&sensor=false";
+    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lon + "&zoom=13&size=300x300&sensor=false";
 
     content.appendChild(img);
   };
@@ -29,3 +29,15 @@ function geoShit() {
 }
 
 geoShit();
+
+// basics
+var lat = '39.28668090308831';
+var lon = '-76.62364196759327';
+var openApiKey = '27beec18e172ad70ec7ca74b22e31896';
+var openApiCall = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&APPID=' + openApiKey;
+
+console.log(openApiCall);
+
+var fcsApiKey = '1b02db75f5fdeb5f829a505b802d5c91';
+var fcsApiCall = 'https://api.forecast.io/forecast/' + fcsApiKey + '/' + lat + ',' + lon;
+console.log(fcsApiCall);
