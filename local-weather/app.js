bce5b0d4932ccaac94ab0f3f0c2c65ec0eb2af3b
Brian Zelip <bzelip@gmail.com>
2016-04-14 22:38:38 -0400

    minor edit for random quotes, new work towards local weather app

--- local-weather/app.js
+++ local-weather/app.js
@@ -1,7 +1,32 @@
-
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(function(position) {
     var text = "latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude;
-    document.getElementById("data").innerHTML = text;
+    lat = position.coords.latitude;
+    long = position.coords.longitude;
+    //document.getElementById("data").innerHTML = text;
   });
+
+  // open weather map api, http://openweathermap.org/current#geo
+  var opnApiURL = 'http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=';
+  var opnApiKey = '38c2076ad830abfaa076a00e9319c7cb';
+  var opnApiCall = opnApiURL.concat(opnApiKey);
+
+  // forecast.io api, https://developer.forecast.io/docs/v2
+  var fcsApiURL = 'https://api.forecast.io/forecast/';
+  var fcsApiKey = 'e8fd3809a962abc2ec4c3a4dba4d2cf8';
+  var fcsApiCall = fcsApiURL.concat(fcsApiKey + '/' + lat + ',' + long);
+
+  console.log(fcsApiCall);
+  var el = document.getElementById("text");
+  el.innerHTML = "<p>hello motherfuckers!!!</p>";
+  
 }
+/*
+if ("geolocation" in navigator) {
+  console.log("yes geolocation is available in the console");
+} else {
+  console.log("geolocation IS NOT available");
+}
+
+console.log(position.coords.latitude);
+*/
