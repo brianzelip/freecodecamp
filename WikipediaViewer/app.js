5d252a7b16168cc11b30606d9c7c2e2b1cb840ae
Brian Zelip <bzelip@gmail.com>
2016-05-03 16:29:51 -0400

    still trying

--- WikipediaViewer/app.js
+++ WikipediaViewer/app.js
@@ -1,3 +1,7 @@
+/*
+
+// The vanilla way to get JSON data that breaks because CORS
+
 function Get(URL){
   var Httpreq = new XMLHttpRequest();
   Httpreq.open("GET",URL,false);
@@ -7,3 +11,14 @@ function Get(URL){
 var wikiURL = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=Albert+Einstein';
 var wikiData = JSON.parse(Get(wikiURL));
 console.log(wikiData.query.search[1]);
+*/
+
+// The JSONP attempt
+$.ajax({
+    dataType: 'jsonp',
+    url: 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=Albert+Einstein',
+    callback: 'test',
+    success : function(json) {
+      console.log(json);
+    }
+})
