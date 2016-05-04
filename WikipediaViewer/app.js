f5e0abcbc1753136888b94f19ceba6b27db3c6d7
Brian Zelip <bzelip@gmail.com>
2016-05-04 07:44:42 -0400

    amfoadsifasf

--- WikipediaViewer/app.js
+++ WikipediaViewer/app.js
@@ -1,3 +1,11 @@
+/*
+
+<<<<<<< HEAD
+Traditional XHR and JSON.parse() method
+=======
+// The vanilla way to get JSON data that breaks because CORS
+>>>>>>> 5d252a7b16168cc11b30606d9c7c2e2b1cb840ae
+
 function Get(URL){
   var Httpreq = new XMLHttpRequest();
   Httpreq.open("GET",URL,false);
@@ -7,3 +15,26 @@ function Get(URL){
 var wikiURL = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=Albert+Einstein';
 var wikiData = JSON.parse(Get(wikiURL));
 console.log(wikiData.query.search[1]);
+*/
+
+// jQuery ajax method
+$.get('https://commons.wikimedia.org/w/api.php?' +
+  $.param({
+      format: 'json',
+      action: 'query',
+      list: 'search',
+      srsearch: 'Albert Einstein',
+      callback: 'test'
+  })).done(function() {
+    console.log("it worked!");
+  });
+=======
+// The JSONP attempt
+$.ajax({
+    dataType: 'jsonp',
+    url: 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=Albert+Einstein',
+    callback: 'test',
+    success : function(json) {
+      console.log(json);
+    }
+})
