74f3b99c8edd38350aacc3f7d753572b13036c46
Brian Zelip <bzelip@gmail.com>
2016-05-05 18:49:40 -0400

    WTF? Got the ajax request working thanks to Stack OVerflow

--- WikipediaViewer/app.js
+++ WikipediaViewer/app.js
@@ -1,10 +1,8 @@
 /*
 
-<<<<<<< HEAD
 Traditional XHR and JSON.parse() method
 =======
-// The vanilla way to get JSON data that breaks because CORS
->>>>>>> 5d252a7b16168cc11b30606d9c7c2e2b1cb840ae
+// The vanilla way to get JSON data; it breaks because CORS.
 
 function Get(URL){
   var Httpreq = new XMLHttpRequest();
@@ -16,7 +14,7 @@ var wikiURL = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=
 var wikiData = JSON.parse(Get(wikiURL));
 console.log(wikiData.query.search[1]);
 */
-
+/*
 // jQuery ajax method
 $.get('https://commons.wikimedia.org/w/api.php?' +
   $.param({
@@ -28,13 +26,15 @@ $.get('https://commons.wikimedia.org/w/api.php?' +
   })).done(function() {
     console.log("it worked!");
   });
-=======
+*/
 // The JSONP attempt
+// adapted from http://stackoverflow.com/a/33460622/2145103
 $.ajax({
     dataType: 'jsonp',
     url: 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=Albert+Einstein',
-    callback: 'test',
     success : function(json) {
-      console.log(json);
+      console.log("Yo, the ajax worked.");
+      results.innerHTML = results.innerHTML + '<code>' + json.query.search[1].title + '</code>';
     }
 })
+// IT WORKS!
