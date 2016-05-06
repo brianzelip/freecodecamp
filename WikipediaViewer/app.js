cc3ce9fb65d3c1c596092bb64e09a0849f9dd251
Brian Zelip <bzelip@gmail.com>
2016-05-06 06:38:16 -0400

    HTMLTextAreaElement was refreshing all along, the issue was the new html was being appended to the bottom of the existing html so i never saw it cause didnt scroll down so thought it wasnt refreshing. duh. working well - almost finished

--- WikipediaViewer/app.js
+++ WikipediaViewer/app.js
@@ -1,11 +1,16 @@
-$.ajax({
+submit.onclick = function() {
+  userInput = document.getElementById('userInput').value;
+  $.ajax({
     dataType: 'jsonp',
-    url: 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=Albert+Einstein',
+    url: 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=' + userInput,
     success : function(json) {
-      console.log("Yo, the ajax worked.");
+      console.log("Yo, the ajax worked");
+      console.log(json.query.search[2]);
+      results.innerHTML = '';
       for (i=0; i<json.query.search.length; i++) {
-        results.innerHTML = results.innerHTML + '<li>' + json.query.search[i].title + '</li>'
+        results.innerHTML = results.innerHTML + '<li><div class="p2 purple hv-purple"><h2 class="h1 mt0">' + json.query.search[i].title + '</h2><p class="mb0">' + json.query.search[i].snippet + '</p></div></li>'
       }
+      userInput.value = '';
     }
-})
-// adapted from http://stackoverflow.com/a/33460622/2145103
+  })// adapted from http://stackoverflow.com/a/33460622/2145103
+}
