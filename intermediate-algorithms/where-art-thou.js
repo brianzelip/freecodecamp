0a50ea7737cb581d0e372503015c19083c6ea674
Brian Zelip <bzelip@gmail.com>
2016-06-16 16:31:42 -0400

    finished up documentation for where-art-thou, starting next challenge

--- intermediate-algorithms/where-art-thou.js
+++ intermediate-algorithms/where-art-thou.js
@@ -6,8 +6,33 @@ Make a function that looks through an array of objects (first argument) and retu
 */
 
 
-//** The for...in solution (which doesn't satisfy the need for accounting for more than one keyvalue pair in source)**
+// SEE the end of this file for documentation of the three Stack Overflow answers submitted to my question about this algorithm.
 
+
+// My for...in solution upgraded with the help of [the solution I checked for the answer to my question on !so about this algorithm](http://stackoverflow.com/a/37845407/2145103).
+
+function whereAreYou(collection, source) {
+  var arr = [];
+
+  for (i=0; i<collection.length; i++) {
+    var matches = true;
+    for (var prop in source) {
+      if (!collection[i].hasOwnProperty(prop) || collection[i][prop] !== source[prop]) {
+        matches = false;
+      }
+    }
+    if (matches) {
+      arr.push(collection[i]);
+    }
+  }
+  console.log('arr is: ' + arr);
+  return arr;
+}
+
+whereAreYou([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "c": 2 }], { "a": 1, "b": 2 });
+
+//** The for...in solution (which doesn't satisfy the need for accounting for more than one keyvalue pair in source)**
+/*
 function whereAreYou(collection, source) {
   // What's in a name?
   var arr = [];
@@ -28,7 +53,7 @@ function whereAreYou(collection, source) {
 }
 
 whereAreYou([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 });
-
+*/
 //end keyvalue pair limited solution
 /*my !SO question post about this algorithm: http://stackoverflow.com/questions/37845249/check-if-any-object-in-an-array-of-objects-contains-all-of-the-key-value-pairs-i
 
@@ -90,5 +115,101 @@ function whereAreYou(collection, source) {
   // Only change code above this line
   return arr;
 }
+
+*/
+
+
+
+
+/*
+[My question about this algoritm on Stack Overflow](http://stackoverflow.com/questions/37845249/check-if-any-object-in-an-array-of-objects-contains-all-of-the-key-value-pairs-i)
+
+
+//[The first, and accepted, answer](http://stackoverflow.com/a/37845407/2145103):
+
+```
+function findObjects(collection, source) {
+    var result = [];
+
+    for (i=0; i<collection.length; i++) {
+        var matches = true;
+        for (var prop in source) {
+            if (!collection[i].hasOwnProperty(prop) || collection[i][prop] !== source[prop]) {
+                matches = false;
+                break;
+            }
+        }
+        if (matches) {
+            result.push(collection[i]);
+        }
+    }
+    return result;
+}
+```
+
+Basically, you have to check ALL properties and make sure they match, and then only then do you add it to your result array.
+
+
+//[The second answer](http://stackoverflow.com/a/37845415/2145103):
+
+You could use some array methods, like Array#map
+
+   The map() method creates a new array with the results of calling a provided function on every element in this array.
+
+and Array#every
+
+   The every() method tests whether all elements in the array pass the test implemented by the provided function.
+
+and get the properties of source first with Object.keys.
+
+   The Object.keys() method returns an array of a given object's own enumerable properties, in the same order as that provided by a for...in loop (the difference being that a for-in loop enumerates properties in the prototype chain as well).
+
+```
+function findObjects(collection, source) {
+   var keys = Object.keys(source);          // get all properties of source
+   return collection.filter(function (c) {  // filter collection
+       return keys.every(function (k) {     // check every key
+           return c[k] === source[k];       // compare value of collection and source
+       });
+   });
+}
+
+console.log(findObjects([{ "a": 1, "b": 2 }, { "a": 1 }, { "b": 2, "c": 2 }], { "a": 1, "b": 2 }))
+```
+
+and
+
+The same in ES6 syntax (read more: Arrow functions)
+    Basically this style is a short writing of
+    function (x) { return y; }
+    became
+              x =>        y
+```
+function findObjects(collection, source) {
+    const keys = Object.keys(source);
+    return collection.filter(c => keys.every(k => c[k] === source[k]));
+}
+
+console.log(findObjects([{ "a": 1, "b": 2 }, { "a": 1 }, { "b": 2, "c": 2 }], { "a": 1, "b": 2 }));
+```
+
+
+//[The third answer](http://stackoverflow.com/a/37846037/2145103):
+
+The solution using Object.keys, Array.forEach and Array.some functions:
+
+```
+function findObjects(arr, source) {
+    var keys = Object.keys(source), result = [];
+    arr.forEach(function(v){
+        !keys.some((k) => source[k] !== v[k]) && result.push(v);
+    });
+
+    return result;
+}
+
+console.log(findObjects([{ "a": 1, "b": 2 }, { "a": 1 }, { "b": 2, "c": 2 }], { "a": 1, "b": 2 }));
+// it will output [{"a":1,"b":2}]
+```
 
 */
