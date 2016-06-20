3f69ed8d9e59db1e61063c3b46e8a18d25c3bf3d
Brian Zelip <bzelip@gmail.com>
2016-06-20 07:49:53 -0400

    got missing letters almost complete

--- intermediate-algorithms/missing-letters.js
+++ intermediate-algorithms/missing-letters.js
@@ -16,10 +16,69 @@ function fearNotLetter(str) {
     return;
   }//make sure input is a string
 
-  str = str.toLowerCase();
+  str = str.toLowerCase();//homogenize input
+  arrCode = [];
+
+  for (i=0; i<str.length; i++) {
+    arrCode.push(str[i].charCodeAt());
+    console.log(arrCode);
+  }
+
+  arrCode.sort(compareNumbers)
+
+  function compareNumbers(a, b) {
+    return a - b;
+  }
+
+  console.log(arrCode);
+
+  var missing = [];
+  for (x=0; x<arrCode.length; x++) {
+    if (x === arrCode.length - 1) {
+      break;
+    } else if ((arrCode[x] + 1) !== arrCode[x+1]) {
+      missing.push((arrCode[x] + 1));
+      console.log('missing now contains: ' + missing);
+    }
+  }
+
+  console.log('-------------------');
+  console.log(arrCode[0+1]);
 
   console.log(str);
   return str;
 }
 
-fearNotLetter("ABCE");
+fearNotLetter("BACE");
+
+
+/*
+
+var next = x + 1;
+console.log('now is index[' + x + '], next is index[' + (x+1) + '].');
+console.log('x is: ' + x);
+console.log('arrCode[x] is: ' + arrCode[x]);
+console.log('arrCode[x+1] is: ' + arrCode[x+1]);
+console.log('arrCode[(x+1)] is: ' + arrCode[(x+1)]);
+
+
+
+infinite loop
+
+else if (start + 1 !== arrCode[x] + 1) {
+  arrCode.push(start + 1);
+  start++;
+  console.log('start is now: ' + start);
+}
+
+*/
+
+
+/*
+REFERENCES
+  - arr.sort(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+  - function compareNumbers(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#Creating_displaying_and_sorting_an_array
+  - str.charCodeAt(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charCodeAt
+  - str.fromCharCode(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/fromCharCode
+
+*/
