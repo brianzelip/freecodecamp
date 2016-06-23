d6c75bf80c4746ca2fe22ccd59642430fa4a65ea
Brian Zelip <bzelip@gmail.com>
2016-06-23 08:04:26 -0400

    solved the sorted-union challenge. That was a fun and easy one.

--- intermediate-algorithms/sorted-union.js
+++ intermediate-algorithms/sorted-union.js
@@ -10,9 +10,25 @@ Check the assertion tests for examples.
 
 function uniteUnique(arr) {
 
-  
+  //console.log('the args are: ' + arguments);
+  //console.log('args lenght is: ' + arguments.length);
 
-  return arr;
+  let uniques = [];//get ready to capture uniques
+
+  for (a=0; a<arguments.length; a++) {//first traverse the arrays in the arguments passed
+    //console.log('argument ' + a + ' is: ' + arguments[a]);
+    for (e=0; e<arguments[a].length; e++) {//now traverse each element of each array passed
+      //console.log('index ' + e + ' of argument ' + a + ' is: ' + arguments[a][e]);
+      if (uniques.indexOf(arguments[a][e]) === -1) {
+        uniques.push(arguments[a][e]);
+        //console.log('uniques was updated with: ' + arguments[a][e]);
+      }
+    }
+  }
+
+  //console.log(uniques);
+
+  return uniques;
 }
 
 uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
@@ -20,6 +36,8 @@ uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
 /*
 REFERENCES
   - Arguments.object: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
+  - let: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/let
+  - arr.indexOf() [they even have an example showing the solution to this challenge (although I figured it out before reading it)]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
   - Array.prototype.reduce(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
 
 */
