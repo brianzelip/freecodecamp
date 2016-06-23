b53ccfcf289de8b9becfe18259e062c0537fe7e3
Brian Zelip <bzelip@gmail.com>
2016-06-23 16:46:47 -0400

    more work

--- intermediate-algorithms/convert-html-entities.js
+++ intermediate-algorithms/convert-html-entities.js
@@ -6,10 +6,16 @@ Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a strin
 function convertHTML(str) {
 
   var newstr = str.replace(/&/i, '&amp;');
+  var newString = str.replace(/(&)(<)(>)(")(')/i, replacer);
+
+  function replacer(p1, p2, p3, p4, p5, string) {
+    return [p1, p2, p3].join(' - ');
+  }
 
   // &colon;&rpar;
-  console.log(newstr);
-  return str;
+  console.log('newstr is: ' + newstr);
+  console.log('newString is: ' + newString);
+  //return str;
 }
 
 convertHTML("Dolce & Gabbana");
@@ -19,9 +25,8 @@ REFERENCES
   - regex: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp
   - html entities: https://dev.w3.org/html5/html-author/charref
   - str.replace(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
-  - regex: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp
   - what is grep: https://kb.iu.edu/d/afiy
   - html entity: https://developer.mozilla.org/en-US/docs/Glossary/Entity
-  - 
+  -
 
 */
