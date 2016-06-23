d4f778f0bccb5af30da85f17a4aa808e346f40dd
Brian Zelip <bzelip@gmail.com>
2016-06-23 08:40:48 -0400

    add a few references

--- intermediate-algorithms/convert-html-entities.js
+++ intermediate-algorithms/convert-html-entities.js
@@ -18,5 +18,10 @@ convertHTML("Dolce & Gabbana");
 REFERENCES
   - regex: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp
   - html entities: https://dev.w3.org/html5/html-author/charref
+  - str.replace(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
+  - regex: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp
+  - what is grep: https://kb.iu.edu/d/afiy
+  - html entity: https://developer.mozilla.org/en-US/docs/Glossary/Entity
+  - 
 
 */
