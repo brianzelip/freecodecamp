36aec8fdd83df03476e25d6f39ae7c36b561e69f
Brian Zelip <bzelip@gmail.com>
2016-06-25 13:37:27 -0400

    solved convert html entities

--- intermediate-algorithms/convert-html-entities.js
+++ intermediate-algorithms/convert-html-entities.js
@@ -5,28 +5,42 @@ Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a strin
 
 function convertHTML(str) {
 
-  var newstr = str.replace(/&/i, '&amp;');
-  var newString = str.replace(/(&)(<)(>)(")(')/i, replacer);
+  var dict = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    '\'': '&apos;'
+  };
 
-  function replacer(p1, p2, p3, p4, p5, string) {
-    return [p1, p2, p3].join(' - ');
+  //console.log(dict['&']);
+
+  var keys = Object.keys(dict);//since you can't access javascript object properties by number for iterating (try console.logging dict[1] for example), you have to make an array of the keys using Object.keys()
+
+  for (i=0; i<str.length; i++) {//for every character in str
+    for (k=0; k<keys.length; k++) {//for every property name in keys, if the str character matches the property name, then replace with that property's value.
+      //console.log("dict." + prop + " = " + dict[prop]);
+      if (str[i] === keys[k]) {
+        //console.log('Houston we have a match! str[i] value is: ' + str[i] + ' and keys[k] value is: ' + keys[k] + ', and the value dict[k] is: ' + dict[keys[k]]);
+        //console.log('AH HEM! keys[k] is: ' + keys[k] + '.\nAH HEM! str[i] is: ' + str[i] + '.\nAH HEM! dict[keys[k]] is: ' + dict[keys[k]] + '.');
+        str = str.replace(str[i], dict[keys[k]]);
+      }
+    }
   }
 
-  // &colon;&rpar;
-  console.log('newstr is: ' + newstr);
-  console.log('newString is: ' + newString);
-  //return str;
+  //console.log('keys.length (ie: the length of the dict object) is: ' + keys.length);
+  //console.log('str is now: ' + str);
+  return str;
 }
 
-convertHTML("Dolce & Gabbana");
+convertHTML("Dolce & Gabbana, not to mention 3 > 2!!");
 
 /*
 REFERENCES
   - regex: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp
   - html entities: https://dev.w3.org/html5/html-author/charref
   - str.replace(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
+  - Object.keys(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
   - what is grep: https://kb.iu.edu/d/afiy
   - html entity: https://developer.mozilla.org/en-US/docs/Glossary/Entity
-  -
-
 */
