6aba7e09c0ebd8163ee549733090147edb693298
Brian Zelip <bzelip@gmail.com>
2016-06-25 13:39:31 -0400

    starting work on spinal-tap-case

---
+++ intermediate-algorithms/spinal-tap-case.js
@@ -0,0 +1,21 @@
+/*
+Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
+*/
+
+
+function spinalCase(str) {
+  // "It's such a fine line between stupid, and clever."
+  // --David St. Hubbins
+  return str;
+}
+
+spinalCase('This Is Spinal Tap');
+
+
+/*
+REFERENCES
+  - regex: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp
+  - str.replace(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
+  - 
+*/
