85de03f07f93d55823da5d38af4e8dde8a7dc000
Brian Zelip <bzelip@gmail.com>
2016-07-02 14:24:44 -0400

    solved spinal tap -- awesome challenge! Its like a filename converter app

--- intermediate-algorithms/spinal-tap-case.js
+++ intermediate-algorithms/spinal-tap-case.js
@@ -1,21 +1,43 @@
 /*
 Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
-
 */
-
 
 function prependDashesToCapitals(str) {
   return str.replace(/[A-Z]/g, '-$&');
 }
 
+function replaceUnderscoresWithDashes(str) {
+  return str.replace(/_/g, '-');
+}
+
+function replaceSpacesWithDashes(str) {
+  return str.replace(/\s/g, '-');
+}
+
+function truncateGroupedDashes(str) {
+  return str.replace(/-+/g, '-');
+}
+
+function trimLeadingDash(str) {
+  return str.replace(/^-/, '');
+}
+
+function toLower(str) {
+  return str.toLowerCase();
+}
+
 function spinalCase(str) {
   str = prependDashesToCapitals(str);
+  str = replaceUnderscoresWithDashes(str);
+  str = replaceSpacesWithDashes(str);
+  str = truncateGroupedDashes(str);
+  str = trimLeadingDash(str);
+  str = toLower(str);
   console.log(str);
   return str;
 }
 
-spinalCase('This Is Spinal Tap');
-
+spinalCase("Teletubbies say Eh-oh");
 
 /*
 REFERENCES
@@ -24,5 +46,12 @@ REFERENCES
   - str.split(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
   - arr.join(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
   - str.indexOf(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf
-  - insertDash: http://stackoverflow.com/a/23196488/2145103
+  - insertDash (decided not to use this approach, but might be a useful future reference): http://stackoverflow.com/a/23196488/2145103
+
+  FUNNY HOW IT ALL CAME DOWN TO:
+    - functions with return statements
+    - str.replace
+    - regex
+
+    THANKS YOU DON'T KNOW JS!!
 */
