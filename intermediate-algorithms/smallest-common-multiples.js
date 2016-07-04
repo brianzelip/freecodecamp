1820d25948b7534283e4dda8c68239f0ed2e2c1a
Brian Zelip <bzelip@gmail.com>
2016-07-04 14:58:25 -0400

    adding my theoretical approach and one of three needed functionalities

--- intermediate-algorithms/smallest-common-multiples.js
+++ intermediate-algorithms/smallest-common-multiples.js
@@ -8,15 +8,41 @@ The range will be an array of two numbers that will not necessarily be in numeri
 e.g. for 1 and 3 - find the smallest common multiple of both 1 and 3 that is evenly divisible by all numbers between 1 and 3.
 */
 
-function smallestCommons(arr) {
+/*
+APPROACH
+1. normalize input (validate and sort values in ascending order)
+2. create arrays of multiples for each number within the input range
+3. return the lowest value that each array contains
+
+*/
+
+function normalize(arr) {
+  for (i=0; i<arr.length; i++) {
+    if (typeof(arr[i]) !== 'number') {
+      console.log('Please enter an array of two positive integers!');
+      return undefined;
+    }
+  }
+
+  function compareNumbers(a, b) {
+    return a - b;
+  }
+
+  arr = arr.sort(compareNumbers);
+  console.log('normalized arr is: [' + arr + ']');
   return arr;
 }
 
 
-smallestCommons([1,5]);
+
+function smallestCommons(arr) {
+  normalize(arr);
+}
+smallestCommons([5,1]);
 
 /*
 REFERENCES
   - least common multiples: https://www.mathsisfun.com/least-common-multiple.html
-  - 
+  - function compareNumbers(a, b): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#Creating_displaying_and_sorting_an_array
+  -
 */
