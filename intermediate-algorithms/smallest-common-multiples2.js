23f496cc212a2cda434fc96a19a388a219441727
Brian Zelip <bzelip@gmail.com>
2016-07-12 07:44:13 -0400

    starting a new file for clean approach

---
+++ intermediate-algorithms/smallest-common-multiples2.js
@@ -0,0 +1,24 @@
+function smallestCommons(arr) {
+  if (typeof(arr) !== 'object' ||
+  arr.length > 2 ||
+  arr[0] < 0 ||
+  arr[1] < 0 ||
+  typeof(arr[0]) !== 'number' ||
+  typeof(arr[1]) !== 'number') {
+    console.log('Please enter an array containing two positive integers!');
+    return;
+  }
+
+  function compareNumbers(a,b) {
+    return a-b;
+  }
+
+  arr.sort(compareNumbers)
+  console.log(arr);
+}
+
+var arr = [1.2];
+
+console.log(typeof(arr));
+
+smallestCommons([22,6]);
