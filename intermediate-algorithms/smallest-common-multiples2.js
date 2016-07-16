6f0db10b22ac9e26fe1468bb62f4b7c706c1254d
Brian Zelip <bzelip@gmail.com>
2016-07-16 15:35:52 -0400

    more work on the different smallest multiple approach

--- intermediate-algorithms/smallest-common-multiples2.js
+++ intermediate-algorithms/smallest-common-multiples2.js
@@ -1,20 +1,27 @@
-function smallestCommons(arr) {
-  if (typeof(arr) !== 'object' ||
-  arr.length > 2 ||
-  arr[0] < 0 ||
-  arr[1] < 0 ||
-  typeof(arr[0]) !== 'number' ||
-  typeof(arr[1]) !== 'number') {
-    console.log('Please enter an array containing two positive integers!');
-    return;
-  }
 
+function normalize(arr) {
   function compareNumbers(a,b) {
     return a-b;
   }
 
-  arr.sort(compareNumbers)
-  console.log(arr);
+  if (typeof(arr) !== 'object' ||
+  arr.length > 2 ||
+  typeof(arr[0]) !== 'number' ||
+  typeof(arr[1]) !== 'number' ||
+  arr[0] < 0 ||
+  arr[1] < 0) {
+    console.log('Please enter an array containing two positive integers!');
+    return;
+  }
+
+  return arr.sort(compareNumbers);
+}
+
+
+function smallestCommons(arr) {
+  normalize(arr);
+  console.log('arr is: ' + arr);
+  return arr;
 }
 
 smallestCommons([22,6]);
